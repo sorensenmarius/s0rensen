8c901b9cbd8d689029537cf27434c1010dcb7a1f
Marius Sørensen <marius.sorensen@smartstudy.no>
2019-06-06 11:21:21 +0200

    Social media logos

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -4,6 +4,10 @@
     <div id="contactDiv">
       <h2><a href="mailto:marius@s0rensen.no" id="mailLink">marius@s0rensen.no</a></h2>
       <h2>+47 909 12 359</h2>
+      <div id="contactIcons">
+        <div class="smLogo"><a href="https://github.com/sorensenmarius" target="_blank"><img src="../assets/github.svg"></a></div>
+        <div class="smLogo"><a href="https://www.linkedin.com/in/sorensenmarius/" target="_blank"><img src="../assets/linkedin.svg"></a></div>
+      </div>
     </div>
   </div>
 </template>
